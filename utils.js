775b89e6230938ef9e490ed4ab6a826962f40542
myekul <somebodynamedmichael@gmail.com>
2025-07-23 00:25:02 -0400

    Generalized assets

---
+++ utils.js
@@ -0,0 +1,30 @@
+function getDateDif(date1, date2) {
+    return Math.floor((date1 - date2) / (100 * 60 * 60 * 24) / 10)
+}
+function daysAgo(dateDif) {
+    dateDif = dateDif.toLocaleString()
+    return dateDif == 0 ? 'Today' : dateDif + ` day${dateDif == 1 ? '' : 's'} ago`
+}
+function toggleVisibility(elem) {
+    const element = document.getElementById(elem)
+    if (element) {
+        if (document.getElementById(elem).style.display == '') {
+            hide(elem)
+        } else {
+            show(elem)
+            return 1
+        }
+    }
+}
+function show(elem) {
+    if (typeof (elem) == 'string') {
+        elem = document.getElementById(elem)
+    }
+    elem.style.display = ''
+}
+function hide(elem) {
+    if (typeof (elem) == 'string') {
+        elem = document.getElementById(elem)
+    }
+    elem.style.display = 'none'
+}
