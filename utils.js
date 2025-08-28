fe2121327b5e1f61a1505b84dc39bd8c7c783e9e
myekul <somebodynamedmichael@gmail.com>
2025-08-28 15:00:39 -0400

    Reverted img fix

--- utils.js
+++ utils.js
@@ -239,8 +239,8 @@ function toggleSidebar() {
         show(sidebar)
     }
 }
-function sharedAssetsImg(src, size = 'auto') {
-    return `<img src='${sharedAssetsURL(src)}' style='height:${size};width:auto'>`
+function sharedAssetsImg(src) {
+    return `<img src='${sharedAssetsURL(src)}'>`
 }
 function sharedAssetsURL(src) {
     return `https://myekul.github.io/shared-assets/images/${src}.png`
