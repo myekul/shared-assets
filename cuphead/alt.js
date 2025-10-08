aec3a4acfaa5adc5bfe4eb7adb206b0e0adb60b5
myekul <somebodynamedmichael@gmail.com>
2025-10-08 15:20:27 -0400

    Updated 1.1 alts

--- cuphead/alt.js
+++ cuphead/alt.js
@@ -2,29 +2,75 @@ const alt = {
     '1.1+': {
         therootpack: [
             {
+                name: '2 Onion EXs',
+                time: 33.86,
+                player: 'ExclamationMarkYT',
+                url: 'https://youtu.be/5XuoQ9-PDu4'
+            },
+            {
                 name: '1 Onion EX',
                 time: 34.36,
-                player: '*ExclamationMarkYT',
+                player: 'ExclamationMarkYT',
                 url: 'https://youtu.be/5XuoQ9-PDu4'
+            }
+        ],
+        ribbyandcroaks: [
+            {
+                name: '5 Flies Regular Skip, Bulls',
+                time: 40.79,
+                player: 'Grondious',
+                url: 'https://youtu.be/-KvAjbtm56E'
             }
         ],
         goopylegrande: [
             {
+                name: 'Sneeze Skip',
+                time: 21.93,
+                player: 'Grondious',
+                url: 'https://youtu.be/lpWutnyKGlY'
+            },
+            {
                 name: 'Lunge',
                 time: 22.60,
-                player: '*Grondious',
+                player: 'Grondious',
                 url: 'https://youtu.be/-ujkzkqRZ8w'
+            }
+        ],
+        hildaberg: [
+            {
+                name: '',
+                time: 59.55,
+                player: 'Quincely0',
+                url: 'https://youtu.be/PThx8scpvck'
             }
         ],
         cagneycarnation: [
             {
+                name: 'Lunge EX',
+                time: 30.77,
+                player: 'Quincely0',
+                url: 'https://youtu.be/aYvapz6VaFc'
+            },
+            {
+                name: 'No Lunge EX',
+                time: 30.77,
+                player: 'Quincely0',
+                url: 'https://youtu.be/_1xtRopi5Qc'
+            },
+            {
                 name: 'Seeds',
                 time: 33.96,
-                player: '*Grondious',
+                player: 'Grondious',
                 url: 'https://youtu.be/wBCzMTzm2dE'
             }
         ],
         baronessvonbonbon: [
+            {
+                name: 'Waffle, Candy Corn, Cupcake',
+                time: 29.87,
+                player: 'Quincely0',
+                url: 'https://youtu.be/jpj_xHYncyk'
+            },
             {
                 name: 'Waffle, Cupcake, Gumball',
                 time: 33.37,
@@ -34,15 +80,51 @@ const alt = {
             {
                 name: 'Candy Corn, Cupcake, Waffle',
                 time: 32.67,
-                player: '*SBDWolf',
+                player: 'SBDWolf',
                 url: 'https://youtu.be/wGOuZEeCbH4'
+            }
+        ],
+        wallywarbles:[
+            {
+                name: '',
+                time: 46.77,
+                player: 'Quincely0',
+                url: 'https://youtu.be/hsDpiF7xC4s'
+            }
+        ],
+        djimmithegreat:[
+            {
+                name: 'Phase 2 Extra Bullets',
+                time: "1:15.83",
+                player: 'SBDWolf',
+                url: 'https://youtu.be/EvywFzRtkuc'
+            },
+            // {
+            //     name: 'Phase 2 No Extra Bullets',
+            //     time: "",
+            //     player: 'SBDWolf',
+            //     url: 'https://youtu.be/EvywFzRtkuc'
+            // }
+        ],
+        beppitheclown:[
+            {
+                name: '',
+                time: 38.26,
+                player: 'Lewzr',
+                url: 'https://youtu.be/diUqcbv3y1g'
             }
         ],
         rumorhoneybottoms: [
             {
+                name: 'Middle',
+                time: 37.01,
+                player: 'ExclamationMarkYT',
+                url: 'https://youtu.be/1u-nj7kF1iY'
+            },
+            {
                 name: 'Orbs',
                 time: 40.92,
-                player: '*Grondious',
+                player: 'Grondious',
                 url: 'https://youtu.be/agOxv114yyc'
             },
             {
@@ -56,57 +138,137 @@ const alt = {
             {
                 name: 'Red Gem',
                 time: '41.46 / 42.47 RTA',
-                player: '*ClipBoardGuy',
+                player: 'ClipBoardGuy',
                 url: 'https://youtu.be/qLbPsCGQo-g'
             },
             {
                 name: 'Blue Gem',
                 time: '41.93 / 42.66 RTA',
-                player: '*ClipBoardGuy',
+                player: 'ClipBoardGuy',
                 url: 'https://youtu.be/UFn71NypSZg'
+            }
+        ],
+        sallystageplay:[
+            {
+                name: '',
+                time: 44.66,
+                player: 'Quincely0',
+                url: 'https://youtu.be/Dz-lTLwn2U4'
             }
         ],
         wernerwerman: [
             {
                 name: 'Parries',
                 time: '34.17 / 34.89 RTA',
-                player: '*SBDWolf',
+                player: 'SBDWolf',
                 url: 'https://youtu.be/izDrJFDhWOk'
+            },
+            {
+                name: 'No Parries',
+                time: '36.83',
+                player: 'Grondious',
+                url: 'https://youtu.be/7SiU5u3AW6U'
+            }
+        ],
+        captainbrineybeard:[
+            {
+                name: '',
+                time: 36.71,
+                player: 'Quincely0',
+                url: 'https://youtu.be/N6EBTHs8yXg'
+            }
+        ],
+        calamaria:[
+            {
+                name: 'Parries',
+                time: 50.96,
+                player: 'Lewzr',
+                url: 'https://youtu.be/WHPWKFNR1wY'
             }
         ],
         phantomexpress: [
             {
+                name: 'Left Cart Final',
+                time: '57.48 / 59.12 RTA',
+                player: 'SBDWolf',
+                url: 'https://youtu.be/L82I_PlmAuw'
+            },
+            {
+                name: '2 Triples, Middle Cart Final',
+                time: '57.70 / 59.34 RTA',
+                player: 'GamerAttack27',
+                url: 'https://youtu.be/Fw05Vx5NOR8'
+            },
+            {
+                name: 'Middle Cart Final',
+                time: '57.84 / 59.48 RTA',
+                player: 'Misterbutter444',
+                url: 'https://youtu.be/dLVsNJ5ElXQ'
+            },
+            {
                 name: 'Safe Phase 1',
                 time: '59.99 / 62.21 RTA',
-                player: '*Grondious',
+                player: 'Grondious',
                 url: 'https://youtu.be/izDrJFDhWOk'
             }
         ],
+        kingdice:[
+            {
+                name: '',
+                time: "1:44.69 / 1:46.47 RTA",
+                player: 'Quincely0',
+                url: 'https://youtu.be/stxsgl3xDsM'
+            }
+        ],
         thedevil: [
-            // {
-            //     name: 'Ring Dragon (No freeze)',
-            //     time: 47,
-            //     player: '*myekul',
-            //     url: ''
-            // },
+            {
+                name: "Spider's Kiss",
+                time: 38.79,
+                player: 'Grondious',
+                url: 'https://youtu.be/FuAtHLS56Ms'
+            },
+            {
+                name: 'Bubbles Dragon',
+                time: 40.87,
+                player: 'Lewzr',
+                url: 'https://youtu.be/z51F-XWMpTA'
+            },
             {
                 name: 'Ring Dragon (Freeze)',
                 time: 42.15,
-                player: '*Lewzr',
+                player: 'Lewzr',
                 url: 'https://youtu.be/SeaoWgXfEbA'
             },
+            // {
+            //     name: 'Ring Dragon (No freeze)',
+            //     time: 46,
+            //     player: 'myekul',
+            //     url: ''
+            // },
+            // {
+            //     name: 'Clap Dragon',
+            //     time: 48,
+            //     player: 'myekul',
+            //     url: ''
+            // },
             {
                 name: 'Dragon First',
                 time: 49.54,
-                player: '*myekul',
+                player: 'myekul',
                 url: 'https://youtu.be/W4lcHJRARJA'
             },
             {
                 name: 'Clap Bubbles Clap Dragon',
                 time: 51.97,
-                player: '*ClipBoardGuy',
+                player: 'ClipBoardGuy',
                 url: 'https://youtu.be/4QkAnWDeX6E'
-            }
+            },
+            // {
+            //     name: 'Clap Ring Clap (Skip)',
+            //     time: 52,
+            //     player: 'myekul',
+            //     url: ''
+            // }
         ]
     },
     'NMG': {
@@ -132,7 +294,7 @@ const alt = {
             {
                 name: '1 Nuke',
                 time: 59.34,
-                player: '*Quincely0',
+                player: 'Quincely0',
                 url: 'https://youtu.be/mFQMrdYSZyA'
             }
         ],
