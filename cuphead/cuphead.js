333449e554bf8f187a5e37da1c28c63f8ca262b8
myekul <somebodynamedmichael@gmail.com>
2025-12-01 00:58:30 -0500

    Moved boss IDs

--- cuphead/cuphead.js
+++ cuphead/cuphead.js
@@ -12,180 +12,178 @@ const bosses = [
         id: 'therootpack',
         name: 'The Root Pack',
         isle: 1,
-        order: 7
+        order: 7,
+        levelID: 6
     },
     {
         id: 'goopylegrande',
         name: 'Goopy Le Grande',
         isle: 1,
-        order: 9
+        order: 9,
+        levelID: 1450863107
     },
     {
         id: 'hildaberg',
         name: 'Hilda Berg',
         isle: 1,
-        order: 10
+        order: 10,
+        levelID: 1449745424
     },
     {
         id: 'cagneycarnation',
         name: 'Cagney Carnation',
         isle: 1,
-        order: 11
+        order: 11,
+        levelID: 1450266910
     },
     {
         id: 'ribbyandcroaks',
         name: 'Ribby and Croaks',
         isle: 1,
-        order: 8
+        order: 8,
+        levelID: 7
     },
     {
         id: 'baronessvonbonbon',
         name: 'Baroness Von Bon Bon',
         isle: 2,
-        order: 12
+        order: 12,
+        levelID: 1451300935
     },
     {
         id: 'beppitheclown',
         name: 'Beppi The Clown',
         isle: 2,
-        order: 15
+        order: 15,
+        levelID: 1456125457
     },
     {
         id: 'djimmithegreat',
         name: 'Djimmi The Great',
         isle: 2,
-        order: 14
+        order: 14,
+        levelID: 1460200177
     },
     {
         id: 'grimmatchstick',
         name: 'Grim Matchstick',
         isle: 2,
-        order: 16
+        order: 16,
+        levelID: 1432722919
     },
     {
         id: 'wallywarbles',
         name: 'Wally Warbles',
         isle: 2,
-        order: 13
+        order: 13,
+        levelID: 1428495827
     },
     {
         id: 'rumorhoneybottoms',
         name: 'Rumor Honeybottoms',
         isle: 3,
-        order: 17
+        order: 17,
+        levelID: 1429976377
     },
     {
         id: 'captainbrineybeard',
         name: 'Captain Brineybeard',
         isle: 3,
-        order: 21
+        order: 21,
+        levelID: 2
     },
     {
         id: 'sallystageplay',
         name: 'Sally Stageplay',
         isle: 3,
-        order: 19
+        order: 19,
+        levelID: 1456740288
     },
     {
         id: 'wernerwerman',
         name: 'Werner Werman',
         isle: 3,
-        order: 20
+        order: 20,
+        levelID: 1430652919
     },
     {
         id: 'drkahlsrobot',
         name: "Dr. Kahl's Robot",
         isle: 3,
-        order: 18
+        order: 18,
+        levelID: 1452935394
     },
     {
         id: 'calamaria',
         name: 'Cala Maria',
         isle: 3,
-        order: 22
+        order: 22,
+        levelID: 1446558823
     },
     {
         id: 'phantomexpress',
         name: 'Phantom Express',
         isle: 3,
-        order: 23
+        order: 23,
+        levelID: 5
     },
     {
         id: 'kingdice',
         name: 'King Dice',
         isle: 4,
-        order: 24
+        order: 24,
+        levelID: 1465296077
     },
     {
         id: 'thedevil',
         name: 'The Devil',
         isle: 4,
-        order: 25
+        order: 25,
+        levelID: 1466688317
     },
     {
         id: 'glumstonethegiant',
         name: 'Glumstone The Giant',
         isle: 5,
-        order: 1
+        order: 1,
+        levelID: 1523429320
     },
     {
         id: 'moonshinemob',
         name: 'Moonshine Mob',
         isle: 5,
-        order: 5
+        order: 5,
+        levelID: 1518081307
     },
     {
         id: 'thehowlingaces',
         name: 'The Howling Aces',
         isle: 5,
-        order: 3
+        order: 3,
+        levelID: 1511943573
     },
     {
         id: 'mortimerfreeze',
         name: 'Mortimer Freeze',
         isle: 5,
-        order: 2
+        order: 2,
+        levelID: 1527591209
     },
     {
         id: 'estherwinchester',
         name: 'Esther Winchester',
         isle: 5,
-        order: 4
+        order: 4,
+        levelID: 1530096313
     },
     {
         id: 'chefsaltbaker',
         name: 'Chef Saltbaker',
         isle: 5,
-        order: 6
+        order: 6,
+        levelID: 1573044456
     }
 ];
-const bossIDs = {
-    therootpack: 6,
-    goopylegrande: 1450863107,
-    hildaberg: 1449745424,
-    cagneycarnation: 1450266910,
-    ribbyandcroaks: 7,
-    baronessvonbonbon: 1451300935,
-    beppitheclown: 1456125457,
-    djimmithegreat: 1460200177,
-    grimmatchstick: 1432722919,
-    wallywarbles: 1428495827,
-    rumorhoneybottoms: 1429976377,
-    captainbrineybeard: 2,
-    sallystageplay: 1456740288,
-    wernerwerman: 1430652919,
-    drkahlsrobot: 1452935394,
-    calamaria: 1446558823,
-    phantomexpress: 5,
-    kingdice: 1465296077,
-    thedevil: 1466688317,
-    glumstonethegiant: 1523429320,
-    moonshinemob: 1518081307,
-    thehowlingaces: 1511943573,
-    mortimerfreeze: 1527591209,
-    estherwinchester: 1530096313,
-    chefsaltbaker: 1573044456
-};
 const mausoleumID = 1481199742
 const runNgunIDs = {
     forestfollies: 1464969490,
