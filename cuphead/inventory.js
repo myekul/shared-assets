2e96e46db8365d4c1e7b23f323707a16aff6e3d6
myekul <somebodynamedmichael@gmail.com>
2025-12-01 07:04:04 -0500

    Reorganized Cuphead assets

---
+++ cuphead/inventory.js
@@ -0,0 +1,216 @@
+const cupheadShots = [
+    {
+        id: 1456773641,
+        name: "PEASHOOTER",
+        subtitle: "EX: Mega Blast",
+        description: "Standard issue weapon. Long range<br>with average damage.",
+        dps: 30,
+        damage: 4,
+        rate: 8,
+        ex: 8.334,
+        exnum: 3,
+        exticks: true,
+        extotal: 25
+    },
+    {
+        id: 1456773649,
+        name: "SPREAD",
+        subtitle: "EX: Eight Way",
+        description: "Short range with great damage - if<br>you can keep close to your target.",
+        dps: 41.33,
+        damage: 1.24,
+        num: 5,
+        rate: 9,
+        ex: 4.3,
+        exnum: 8,
+        extotal: 34.4
+    },
+    {
+        id: 1460621839,
+        name: "CHASER",
+        subtitle: "EX: Chaos Orbit",
+        description: "Long range with below-average<br>damage. No aiming required.",
+        dps: 17.1,
+        damage: 2.85,
+        rate: 10,
+        ex: 7,
+        exnum: 4,
+        extotal: 28
+    },
+    {
+        id: 1467024095,
+        name: "LOBBER",
+        subtitle: "EX: Kablooey",
+        description: "Medium range and good damage with<br>a slower rate of fire.",
+        dps: 33.14,
+        damage: 11.6,
+        rate: 21,
+        ex: 28
+    },
+    {
+        id: 1466416941,
+        name: "CHARGE",
+        subtitle: "EX: Radial Barrage",
+        description: "Hold attack to increase damage. No<br>rapid fire, so precision is key.",
+        dps: `22.5 / 35.84`,
+        damage: 6,
+        rate: 16,
+        damage2: 46,
+        rate2: 61,
+        ex: 26
+    },
+    {
+        id: 1466518900,
+        name: "ROUNDABOUT",
+        subtitle: "EX: Jumbo Rebound",
+        description: "Great coverage with average damage.<br>Aim backward for maximum range.",
+        dps: 31.875,
+        damage: 8.5,
+        rate: 16,
+        ex: 5,
+        exnum: 7,
+        exticks: true,
+        extotal: 35
+    },
+    {
+        id: 1614768724,
+        name: "CRACKSHOT",
+        subtitle: "EX: P. Turret",
+        description: "Straight shot with good damage. Breaks<br>into weaker aimed projectiles.",
+        dps: `30.17 / 19.14`,
+        damage: `10.56 / 6.7`,
+        rate: 21,
+        ex: 3.5,
+        exnum: '6*',
+        extotal: 21
+    },
+    {
+        id: 1487081743,
+        name: "CONVERGE",
+        subtitle: "EX: Electro-Bolt",
+        description: "Full-screen piercing 3-way shot. Hold<br>lock button to narrow spread.",
+        dps: 32.04,
+        damage: 2.67,
+        num: 3,
+        rate: 15,
+        ex: 21
+    },
+    {
+        id: 1568276855,
+        name: "TWIST-UP",
+        subtitle: "EX: Cyclone Spiral",
+        description: "Rapid-fire shot with arced pathway and<br>average damage.",
+        dps: 32.26,
+        damage: 2.33,
+        num: 3,
+        rate: 13,
+        ex: 8,
+        exnum: 5,
+        exticks: true,
+        extotal: 40
+    },
+    {
+        name: "ARC",
+        subtitle: "",
+        description: ""
+    },
+    {
+        name: "EXPLODER",
+        subtitle: "",
+        description: ""
+    }
+]
+const cupheadSupers = [
+    {
+        id: 1456815409,
+        name: "SUPER ART I",
+        subtitle: "Energy Beam",
+        description: "A devastating attack spills from your<br>head. Horizontal only (ground or air)."
+    },
+    {
+        id: 1495012282,
+        name: "SUPER ART II",
+        subtitle: "Invincibility",
+        description: "Cross the astral plane to become<br>invulnerable for a short time."
+    },
+    {
+        id: 1467617939,
+        name: "SUPER ART III",
+        subtitle: "Giant Ghost",
+        description: "Maneuver your spirit and body<br>simultaneously for maximum damage."
+    },
+    {
+        id: 2147483647,
+        name: "-",
+        subtitle: "",
+        description: ""
+    }
+];
+const cupheadCharms = [
+    {
+        id: 1460832742,
+        name: "HEART",
+        subtitle: "Extra Hit Point",
+        description: "Adds an additional hit point but<br>lightly weakens your attack power."
+    },
+    {
+        id: 1460880866,
+        name: "COFFEE",
+        subtitle: "Autofill Meter",
+        description: "Super meter continuously fills - in<br>addition to what you earn."
+    },
+    {
+        id: 1461001046,
+        name: "SMOKE BOMB",
+        subtitle: "Invisible Dash",
+        description: "You will not take damage during a<br>dash. A great defense maneuver."
+    },
+    {
+        id: 1487051212,
+        name: "P. SUGAR",
+        subtitle: "Automatic Parry",
+        description: "The first parry move is automatic -<br>all you need to do is jump."
+    },
+    {
+        id: 1500641115,
+        name: "TWIN HEART",
+        subtitle: "Extra Hit Points",
+        description: "Adds two additional hit points but<br>weakens your attack power."
+    },
+    {
+        id: 1500621999,
+        name: "WHETSTONE",
+        subtitle: "Parry Attack",
+        description: "Your first parry move doubles as a<br>damaging axe attack."
+    },
+    {
+        id: 1522153206,
+        name: "ASTRAL COOKIE",
+        subtitle: "Play As Ms. Chalice",
+        description: "Grants access to her double jump, dash<br>parry, and invincible roll."
+    },
+    {
+        id: 1569309672,
+        name: "DIVINE RELIC",
+        subtitle: "Bestows Power",
+        description: "Wondrous talisman that grants a variety<br>of abilities."
+    },
+    {
+        id: 1568891766,
+        name: "HEART RING",
+        subtitle: "Parry for HP",
+        description: "Gain 1 HP on your first, third, and sixth<br>parry."
+    },
+    {
+        id: 1487056728,
+        name: "SHIELD",
+        subtitle: "",
+        description: ""
+    },
+    {
+        id: 2147483647,
+        name: "-",
+        subtitle: "",
+        description: ""
+    }
+];
